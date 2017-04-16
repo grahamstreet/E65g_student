3b25aba5baaea172ef2c1e2bc63a4b0983d003cf
Graham Street <gstreet@Grahams-MacBook-Pro.local>
2017-04-15 21:29:48 -0400

    problem 1

--- Assignment4/Assignment4/SimulationViewController.swift
+++ Assignment4/Assignment4/SimulationViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SimulationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
